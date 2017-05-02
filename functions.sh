a90e8757ab240559cfe173915af2b5d35adb9492
Oliv4945 <oliv4945@gmail.com>
2017-05-02 16:30:42 +0200

    Add local keyword to status variable

--- functions.sh
+++ functions.sh
@@ -5,7 +5,7 @@ jv_pg_gm_check_traffic() {
     
     if [[ -n "$jv_pg_gm_api_key" ]]; then
         local json=$(curl -s -G "https://maps.googleapis.com/maps/api/directions/json" --data "language=fr" --data-urlencode "origin=$jv_pg_gm_city_from" --data-urlencode "destination=$jv_pg_gm_city_to" --data "traffic_model=best_guess" --data "departure_time=now" --data-urlencode "key=$jv_pg_gm_api_key")
-        status=$(echo "$json" | jq -r '.status')
+        local status=$(echo "$json" | jq -r '.status')
     else
         echo $(jv_pg_gm_invalid_key_locale)
         exit
